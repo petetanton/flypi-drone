997ad6dd126a038833ee94d1f010c268d7762057
petetanton <pete.tanton@bbc.co.uk>
2016-10-22 21:52:26 +0100

    tidy up Main

--- src/main/java/uk/flypi/drone/Main.java
+++ src/main/java/uk/flypi/drone/Main.java
@@ -2,12 +2,15 @@ package uk.flypi.drone;
 
 import uk.flypi.drone.http.HttpServer;
 
+import java.util.concurrent.TimeoutException;
+
 public class Main {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws TimeoutException, InterruptedException {
         System.out.println("starting up.\nPlease wait...");
         final HttpServer httpServer = new HttpServer();
         httpServer.start();
         System.out.println("web server started");
     }
+
 }
